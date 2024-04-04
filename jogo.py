028feaf9a482ff833a901dbcea30739e36712326
gabrielgmathias <140728950+gabrielgmathias@users.noreply.github.com>
2024-04-04 09:57:42 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,14 @@
+ import Forca
+ import adivinhacao
+ 
+ def escolhe_jogo():
+     print("********************************")
+     print("******Escolha seu jogo!*********")
+     print("********************************")
+     
+     print("(1) Forca (2) adivinhação")
+     
+     jogo = int(input("qual jogo?"))
+     
+     if(jogo)
+     
